1b904313ab896206a384825d476a6e357c682b36
orisabiyi <orisabiyidavid@gmail.com>
2025-03-17 16:31:47 +0100

    Add package.json and implement particle effects on the login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 import { LoginForm } from "@/components/login-form";
 import Image from "next/image";
-import Logo from '@/public/images/Logo.svg'
+import Logo from "@/public/images/Logo.svg";
 import { useCallback } from "react";
-import Particles from "@tsparticles/react";
+import Particles from "react-particles";
 import type { Container, Engine } from "tsparticles-engine";
 import { loadSlim } from "tsparticles-slim";
 
@@ -13,13 +13,16 @@ export default function LoginPage() {
     await loadSlim(engine);
   }, []);
 
-  const particlesLoaded = useCallback(async (container: Container | undefined) => {
-    console.log(container);
-  }, []);
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      console.log(container);
+    },
+    []
+  );
 
   return (
     <section className="grid min-h-svh lg:grid-cols-7">
-      <div className="flex flex-col items-center justify-center gap-10 col-span-2 p-6 md:p-14">
+      <div className="flex flex-col items-center justify-center gap-10 col-span-2 bg-white p-6 md:p-14">
         <figure>
           <Image src={Logo} alt="Niger Delta Logo" />
         </figure>
@@ -27,83 +30,83 @@ export default function LoginPage() {
         <LoginForm />
       </div>
 
-      <div className="relative hidden bg-muted col-span-5 lg:block">
-      <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "#0d47a1",
-                    },
+      <div className="relative hidden bg-muted col-span-5 lg:block overflow-hidden h-full -z-10">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#2ec4bf",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
                 },
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
-                        },
-                        resize: {
-                          enable: true,
-                          delay: 0,
-                        },
-                    },
-                    modes: {
-                        push: {
-                            quantity: 4,
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
                 },
-                particles: {
-                    color: {
-                        value: "#ffffff",
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 6,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 5 },
-                    },
+                resize: {
+                  enable: true,
+                  delay: 0,
                 },
-                detectRetina: true,
-            }}
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 0.5,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 6,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 3 },
+              },
+            },
+            detectRetina: true,
+          }}
         />
       </div>
     </section>
