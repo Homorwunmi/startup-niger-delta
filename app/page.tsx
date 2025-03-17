8d5219b8bf864590c2704a860facf85de6a35971
orisabiyi <orisabiyidavid@gmail.com>
2025-03-17 16:39:02 +0100

    Refactor particle effects into a separate component for improved code organization

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,9 @@
 import { LoginForm } from "@/components/login-form";
 import Image from "next/image";
 import Logo from "@/public/images/Logo.svg";
-import { useCallback } from "react";
-import Particles from "react-particles";
-import type { Container, Engine } from "tsparticles-engine";
-import { loadSlim } from "tsparticles-slim";
+import Particle from "@/components/particle";
 
 export default function LoginPage() {
-  const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-    await loadSlim(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(
-    async (container: Container | undefined) => {
-      console.log(container);
-    },
-    []
-  );
 
   return (
     <section className="grid min-h-svh lg:grid-cols-7">
@@ -30,85 +16,7 @@ export default function LoginPage() {
         <LoginForm />
       </div>
 
-      <div className="relative hidden bg-muted col-span-5 lg:block overflow-hidden h-full -z-10">
-        <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            background: {
-              color: {
-                value: "#2ec4bf",
-              },
-            },
-            fpsLimit: 120,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                resize: {
-                  enable: true,
-                  delay: 0,
-                },
-              },
-              modes: {
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: "#ffffff",
-              },
-              links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 0.5,
-              },
-              move: {
-                direction: "none",
-                enable: true,
-                outModes: {
-                  default: "bounce",
-                },
-                random: false,
-                speed: 6,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 80,
-              },
-              opacity: {
-                value: 0.5,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                value: { min: 1, max: 3 },
-              },
-            },
-            detectRetina: true,
-          }}
-        />
-      </div>
+      <Particle />
     </section>
   );
 }
