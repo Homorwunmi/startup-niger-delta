6fcdf495538751e788d693e77a033413acf19df5
orisabiyi <orisabiyidavid@gmail.com>
2025-04-12 20:58:50 +0100

    feat: enhance testimonial slider with star ratings and quotation mark

--- components/home/testimonial.tsx
+++ components/home/testimonial.tsx
@@ -8,6 +8,8 @@ import React from "react";
 import Slider from "react-slick";
 
 import TestimonialImage from "@/public/images/testimonial.svg";
+import QuoteImage from "@/public/images/quotation.svg";
+import { IoMdStar } from "react-icons/io";
 
 function SimpleSlider() {
   const name = [
@@ -24,33 +26,51 @@ function SimpleSlider() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 2500,
   };
   return (
-    <ul className="slider-container absolute top-1/2 left-10 transform -translate-y-1/2 h-1/2 w-3/5 bg-white z-10">
+    <ul className="slider-container absolute top-1/2 left-10 transform -translate-y-1/2 h-1/2 w-3/5 bg-white shadow-lg z-10">
       <Slider {...settings}>
         {Array(4)
           .fill(null)
           .map((_, i) => (
             <li
               key={i}
-              className="!flex flex-col items-stretch justify-center gap-4 h-full mt-12 px-10 py-4"
-            >
+              className="!flex flex-col items-stretch justify-center gap-4 h-full mt-12 px-10 py-4 relative">
               <div className="flex items-center gap-5">
                 <figure className="w-14 h-14 rounded-full overflow-hidden">
                   <Image
                     src={TestimonialImage}
-                    alt=""
+                    alt="testimonial-image"
                     className="w-full h-full rounded-full"
                   />
                 </figure>
                 <div>
+                  <figure className="flex items-center">
+                    {Array(5)
+                      .fill(null)
+                      .map((__, index) => (
+                        <IoMdStar
+                          key={index}
+                          size={22}
+                          className="text-custom-orange mb-4"
+                        />
+                      ))}
+                  </figure>
                   <h3 className="text-2xl font-semibold">{name[i]}</h3>
                   <p className="text-custom-orange text-lg font-semibold">
                     Head of Linie House Group
                   </p>
                 </div>
               </div>
+
+              <figure className="absolute top-0 right-10 w-20 h-20">
+                <Image
+                  src={QuoteImage}
+                  alt="quotation-mark"
+                  className="w-full h-full"
+                />
+              </figure>
 
               <p className="text-xl text-gray-500">
                 SNG serves as an ideal platform for showcasing, connecting, and
@@ -75,8 +95,7 @@ export default function Testimonial() {
         style={{
           backgroundImage: `url("/home/ecosystem-bg.svg")`,
           backgroundBlendMode: "soft-light",
-        }}
-      >
+        }}>
         <h2 className="text-4xl text-center text-white font-semibold flex flex-col items-end gap-3">
           <span className="uppercase">Testimonies.</span>
           <span className="w-1/3 h-1 bg-custom-orange inline-block" />
