6dd1aa521262048e47d5ac723a313febd1fea874
Homorwunmi <omotayomowunmi@gmail.com>
2025-04-12 18:58:16 +0100

    Work in progress

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 export const app = initializeApp({
   apiKey: "AIzaSyAhrV0PrLYajbV6urlFoVoxR2PPm7uyaZ0",
@@ -17,3 +18,4 @@ export const app = initializeApp({
 // Initialize Firebase
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
--- src/onboarding.js
+++ src/onboarding.js
@@ -1,8 +1,22 @@
-import { db, auth } from "./config.js";
-import { doc, setDoc  } from "firebase/firestore";
+import { db, auth, storage } from "./config.js";
+import { doc, setDoc  } from "firebase/firestore";\
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 
 export const onboardingRegistration = async (regType, regSection, data) => {
     // Add a new document in collection "cities"
     await setDoc(doc(db, `onboarding/${auth.currentUser.uid}/${regType}`, regSection), data);
+}
+
+export const uploadIdentification = async (cacFile, logoFile) => {
+    const cacRef = ref(storage, `founder-identification/${auth.currentUser.uid}/CAC.png`);
+    const logoRef = ref(storage, `founder-identification/${auth.currentUser.uid}/logo.png`);
+
+    uploadBytes(cacRef, cacFile).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+    });
+    uploadBytes(logoRef, logoFile).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+    });
+    return url;
 }
