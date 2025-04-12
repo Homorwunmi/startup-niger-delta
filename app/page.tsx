799a5d90773821c796508e8d4412ffe68d1d7cf4
orisabiyi <orisabiyidavid@gmail.com>
2025-04-12 12:59:16 +0100

    feat(style): create and style recent news section

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,7 @@ import headerBg from "@/public/images/header-bg.svg";
 import { Ecosystem, InvestmentList, Partners, resourcesList } from "@/lib/home";
 import Investment from "@/public/home/investment.svg";
 import Link from "next/link";
+import RecentNews from "@/components/home/recent-news";
 
 export default function Page() {
   const form = useForm<Subscription>({
@@ -54,8 +55,7 @@ export default function Page() {
       <main>
         <NavigationMenu
           className="py-4 px-20 text-green-900 font-medium border-b-2 border-green-900 border-opacity-50"
-          style={{ maxWidth: "100%" }}
-        >
+          style={{ maxWidth: "100%" }}>
           <NavigationMenuList className="">
             <figure className="mr-auto">
               <Image src={Logo} alt="Niger Delta Logo" />
@@ -64,8 +64,7 @@ export default function Page() {
             <NavigationMenuItem className="flex items-center gap-8 text-custom-green">
               <NavigationMenuLink
                 href="/home"
-                className="hover:bg-transparent p-0"
-              >
+                className="hover:bg-transparent p-0">
                 Home
               </NavigationMenuLink>
               <NavigationMenu className="hover:bg-transparent hover:p-0 p-0">
@@ -73,8 +72,7 @@ export default function Page() {
               </NavigationMenu>
               <NavigationMenuLink
                 href="/funding"
-                className="hover:bg-transparent p-0"
-              >
+                className="hover:bg-transparent p-0">
                 Funding
               </NavigationMenuLink>
               <NavigationMenuLink className="hover:bg-transparent p-0">
@@ -94,14 +92,12 @@ export default function Page() {
             <NavigationMenuItem className="flex items-center gap-4 ml-auto">
               <NavigationMenuLink
                 href="/login"
-                className="underline hover:bg-transparent p-0"
-              >
+                className="underline hover:bg-transparent p-0">
                 Login
               </NavigationMenuLink>
               <NavigationMenuLink
                 href="/sign-up"
-                className="gradient-button w-30 text-center"
-              >
+                className="gradient-button w-30 text-center">
                 Get Started
               </NavigationMenuLink>
             </NavigationMenuItem>
@@ -139,8 +135,7 @@ export default function Page() {
                         </FormControl>
                         <Button
                           type="submit"
-                          className="gradient-button rounded-l-none py-5 px-8 w-30"
-                        >
+                          className="gradient-button rounded-l-none py-5 px-8 w-30">
                           Subscribe
                         </Button>
                       </div>
@@ -185,8 +180,7 @@ export default function Page() {
                   backgroundImage: `url("/home/ecosystem-bg.svg")`,
                   backgroundSize: "cover",
                   backgroundBlendMode: "overlay",
-                }}
-              >
+                }}>
                 <div
                   className={`absolute top-0 w-4/5 h-3 ${(index + 1) % 2 !== 0 ? "bg-light-custom-green" : "bg-custom-green"}`}
                 />
@@ -201,13 +195,11 @@ export default function Page() {
                 <CardHeader className="relative w-full gap-3">
                   <CardTitle
                     className="font text-3xl"
-                    style={{ fontFamily: "Times New Roman, serif" }}
-                  >
+                    style={{ fontFamily: "Times New Roman, serif" }}>
                     {item.title}
                   </CardTitle>
                   <CardDescription
-                    className={`text-sm font-poppins leading-5 ${(index + 1) % 2 !== 0 ? "text-white" : "text-custom-green"}`}
-                  >
+                    className={`text-sm font-poppins leading-5 ${(index + 1) % 2 !== 0 ? "text-white" : "text-custom-green"}`}>
                     {item.description}
                   </CardDescription>
                 </CardHeader>
@@ -236,8 +228,7 @@ export default function Page() {
             backgroundImage: `url("/home/ecosystem-bg.svg")`,
             backgroundSize: "cover",
             backgroundBlendMode: "overlay",
-          }}
-        >
+          }}>
           <div className="flex flex-col items-start gap-6 text-white">
             <h2 className="text-5xl flex flex-col items-start gap-3 font-poppins">
               <span>Investment &</span>
@@ -261,8 +252,7 @@ export default function Page() {
             {InvestmentList.map((item) => (
               <li
                 key={item.title}
-                className="flex flex-col items-start gap-4 bg-custom-green-1 bg-opacity-50 px-5 pt-12 pb-5 rounded-t-3xl w-1/5"
-              >
+                className="flex flex-col items-start gap-4 bg-custom-green-1 bg-opacity-50 px-5 pt-12 pb-5 rounded-t-3xl w-1/5">
                 <figure className="w-16 h-16">
                   <Image
                     src={item.icon}
@@ -312,8 +302,7 @@ export default function Page() {
           style={{
             backgroundImage: `url("/home/ecosystem-bg.svg")`,
             backgroundBlendMode: "soft-light",
-          }}
-        >
+          }}>
           <div className="flex flex-col items-start gap-5 text-white w-3/5">
             <h2 className="text-5xl font-medium flex flex-col items-start gap-3 font-poppins">
               Resources.
@@ -339,8 +328,7 @@ export default function Page() {
             {resourcesList.map((resource) => (
               <li
                 key={resource.title}
-                className="flex items-center gap-4 bg-gray-100 py-2 px-3 rounded-lg"
-              >
+                className="flex items-center gap-4 bg-gray-100 py-2 px-3 rounded-lg">
                 <figure className="w-14 h-14">
                   <Image
                     src={resource.img}
@@ -364,16 +352,16 @@ export default function Page() {
           </ul>
         </section>
 
-        <section>Hello</section>
+        <RecentNews />
       </main>
 
+      {/* Footer */}
       <footer
         className="grid grid-cols-8 gap-10 p-20 text-white bg-custom-green relative"
         style={{
           backgroundImage: `url("/home/ecosystem-bg.svg")`,
           backgroundBlendMode: "soft-light",
-        }}
-      >
+        }}>
         <div className="absolute top-0 left-0 w-full h-2 bg-custom-orange" />
 
         <section className="col-span-3 flex flex-col items-stretch gap-8">
---
+++ components/home/recent-news.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React, { useState } from "react";
+
+import { NewsType } from "@/types/Home";
+import NewsImage1 from "@/public/images/news-image-1.svg";
+import NewsImage2 from "@/public/images/news-image-2.svg";
+import NewsImage3 from "@/public/images/news-image-3.svg";
+
+import Image from "next/image";
+
+import { Button } from "../ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+
+export default function RecentNews(): React.JSX.Element {
+  const [isActive, setIsActive] = useState<NewsType>("latest");
+  const images = [NewsImage1, NewsImage2, NewsImage3];
+
+  function handleSetActive(type: NewsType) {
+    setIsActive(type);
+  }
+
+  return (
+    <section className="flex flex-col items-stretch gap-10 p-20 bg-gray-200">
+      <div className="flex items-end justify-between w-full">
+        <h2 className="flex flex-col item-start gap-2 text-4xl">
+          <span>Recent News.</span>
+          <span className="w-1/2 h-1 bg-custom-orange" />
+        </h2>
+
+        <ul className="flex items-center gap-4">
+          <li>
+            <Button
+              type="button"
+              onClick={() => handleSetActive("latest")}
+              className={`rounded-full px-10 shadow-none ${isActive === "latest" ? "text-white bg-custom-green hover:bg-custom-green" : "text-custom-green border-1 border-custom-green bg-transparent hover:bg-transparent"}`}>
+              Latest
+            </Button>
+          </li>
+          <li>
+            <Button
+              type="button"
+              onClick={() => handleSetActive("news")}
+              className={`rounded-full px-10 shadow-none ${isActive === "news" ? "text-white bg-custom-green hover:bg-custom-green" : "text-custom-green border-1 border-custom-green bg-transparent hover:bg-transparent"}`}>
+              News
+            </Button>
+          </li>
+          <li>
+            <Button
+              type="button"
+              onClick={() => handleSetActive("funding")}
+              className={`rounded-full px-10 shadow-none ${isActive === "funding" ? "text-white bg-custom-green hover:bg-custom-green" : "text-custom-green border-1 border-custom-green bg-transparent hover:bg-transparent"}`}>
+              Funding
+            </Button>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex items-center w-full gap-6">
+        {Array(3)
+          .fill(null)
+          .map((_, i) => (
+            <Card
+              className="w-1/3 shadow-none bg-transparent rounded-none gap-2"
+              key={`card-${i}`}>
+              <CardHeader className="px-0">
+                <CardTitle className="text-xl">
+                  How collaboration makes us better business person
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="w-full px-0 flex flex-col items-stretch gap-2">
+                <figure className="w-full">
+                  <Image src={images[i]} alt="news-image" className="w-full" />
+                </figure>
+                <p className="flex items-center justify-between">
+                  <span>By Admin, NDS</span>
+                  <span>Jan 6, 2024 - 2 min Read</span>
+                </p>
+                <p className="w-full">
+                  Lorem ipsum dolor sit amet consectetur. Ont Condimentum
+                  adipiscing at iaculis m wqwa adiscing convallis ut feugiat
+                  morbi. Indo...
+                </p>
+              </CardContent>
+            </Card>
+          ))}
+      </div>
+    </section>
+  );
+}
