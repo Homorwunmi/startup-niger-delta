6a188d0feb68da565b2bead3611dbbd49a73cf67
orisabiyi <orisabiyidavid@gmail.com>
2025-04-12 15:43:13 +0100

    feat(style): style the events component, create vertical slider carousel component and update its content

--- app/page.tsx
+++ app/page.tsx
@@ -41,6 +41,7 @@ import { Ecosystem, InvestmentList, Partners, resourcesList } from "@/lib/home";
 import Investment from "@/public/home/investment.svg";
 import Link from "next/link";
 import RecentNews from "@/components/home/recent-news";
+import Events from "@/components/home/events";
 
 export default function Page() {
   const form = useForm<Subscription>({
@@ -55,8 +56,7 @@ export default function Page() {
       <main>
         <NavigationMenu
           className="py-4 px-20 text-green-900 font-medium border-b-2 border-green-900 border-opacity-50"
-          style={{ maxWidth: "100%" }}
-        >
+          style={{ maxWidth: "100%" }}>
           <NavigationMenuList className="">
             <figure className="mr-auto">
               <Image src={Logo} alt="Niger Delta Logo" />
@@ -65,8 +65,7 @@ export default function Page() {
             <NavigationMenuItem className="flex items-center gap-8 text-custom-green">
               <NavigationMenuLink
                 href="/home"
-                className="hover:bg-transparent p-0"
-              >
+                className="hover:bg-transparent p-0">
                 Home
               </NavigationMenuLink>
               <NavigationMenu className="hover:bg-transparent hover:p-0 p-0">
@@ -74,8 +73,7 @@ export default function Page() {
               </NavigationMenu>
               <NavigationMenuLink
                 href="/funding"
-                className="hover:bg-transparent p-0"
-              >
+                className="hover:bg-transparent p-0">
                 Funding
               </NavigationMenuLink>
               <NavigationMenuLink className="hover:bg-transparent p-0">
@@ -95,14 +93,12 @@ export default function Page() {
             <NavigationMenuItem className="flex items-center gap-4 ml-auto">
               <NavigationMenuLink
                 href="/login"
-                className="underline hover:bg-transparent p-0"
-              >
+                className="underline hover:bg-transparent p-0">
                 Login
               </NavigationMenuLink>
               <NavigationMenuLink
                 href="/sign-up"
-                className="gradient-button w-30 text-center"
-              >
+                className="gradient-button w-30 text-center">
                 Get Started
               </NavigationMenuLink>
             </NavigationMenuItem>
@@ -140,8 +136,7 @@ export default function Page() {
                         </FormControl>
                         <Button
                           type="submit"
-                          className="gradient-button rounded-l-none py-5 px-8 w-30"
-                        >
+                          className="gradient-button rounded-l-none py-5 px-8 w-30">
                           Subscribe
                         </Button>
                       </div>
@@ -186,8 +181,7 @@ export default function Page() {
                   backgroundImage: `url("/home/ecosystem-bg.svg")`,
                   backgroundSize: "cover",
                   backgroundBlendMode: "overlay",
-                }}
-              >
+                }}>
                 <div
                   className={`absolute top-0 w-4/5 h-3 ${(index + 1) % 2 !== 0 ? "bg-light-custom-green" : "bg-custom-green"}`}
                 />
@@ -202,13 +196,11 @@ export default function Page() {
                 <CardHeader className="relative w-full gap-3">
                   <CardTitle
                     className="font text-3xl"
-                    style={{ fontFamily: "Times New Roman, serif" }}
-                  >
+                    style={{ fontFamily: "Times New Roman, serif" }}>
                     {item.title}
                   </CardTitle>
                   <CardDescription
-                    className={`text-sm font-poppins leading-5 ${(index + 1) % 2 !== 0 ? "text-white" : "text-custom-green"}`}
-                  >
+                    className={`text-sm font-poppins leading-5 ${(index + 1) % 2 !== 0 ? "text-white" : "text-custom-green"}`}>
                     {item.description}
                   </CardDescription>
                 </CardHeader>
@@ -237,8 +229,7 @@ export default function Page() {
             backgroundImage: `url("/home/ecosystem-bg.svg")`,
             backgroundSize: "cover",
             backgroundBlendMode: "overlay",
-          }}
-        >
+          }}>
           <div className="flex flex-col items-start gap-6 text-white">
             <h2 className="text-5xl flex flex-col items-start gap-3 font-poppins">
               <span>Investment &</span>
@@ -262,8 +253,7 @@ export default function Page() {
             {InvestmentList.map((item) => (
               <li
                 key={item.title}
-                className="flex flex-col items-start gap-4 bg-custom-green-1 bg-opacity-50 px-5 pt-12 pb-5 rounded-t-3xl w-1/5"
-              >
+                className="flex flex-col items-start gap-4 bg-custom-green-1 bg-opacity-50 px-5 pt-12 pb-5 rounded-t-3xl w-1/5">
                 <figure className="w-16 h-16">
                   <Image
                     src={item.icon}
@@ -313,8 +303,7 @@ export default function Page() {
           style={{
             backgroundImage: `url("/home/ecosystem-bg.svg")`,
             backgroundBlendMode: "soft-light",
-          }}
-        >
+          }}>
           <div className="flex flex-col items-start gap-5 text-white w-3/5">
             <h2 className="text-5xl font-medium flex flex-col items-start gap-3 font-poppins">
               Resources.
@@ -340,8 +329,7 @@ export default function Page() {
             {resourcesList.map((resource) => (
               <li
                 key={resource.title}
-                className="flex items-center gap-4 bg-gray-100 py-2 px-3 rounded-lg"
-              >
+                className="flex items-center gap-4 bg-gray-100 py-2 px-3 rounded-lg">
                 <figure className="w-14 h-14">
                   <Image
                     src={resource.img}
@@ -366,6 +354,8 @@ export default function Page() {
         </section>
 
         <RecentNews />
+
+        <Events />
       </main>
 
       {/* Footer */}
@@ -374,8 +364,7 @@ export default function Page() {
         style={{
           backgroundImage: `url("/home/ecosystem-bg.svg")`,
           backgroundBlendMode: "soft-light",
-        }}
-      >
+        }}>
         <div className="absolute top-0 left-0 w-full h-2 bg-custom-orange" />
 
         <section className="col-span-3 flex flex-col items-stretch gap-8">
--- components/home/events.tsx
+++ components/home/events.tsx
@@ -1,21 +1,103 @@
 "use client";
 
-import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+import React from "react";
+import Slider from "react-slick";
+
+import NewsImage1 from "@/public/images/news-image-1.svg";
+import NewsImage2 from "@/public/images/news-image-2.svg";
+import NewsImage3 from "@/public/images/news-image-3.svg";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+function MultipleItems() {
+  const images = [NewsImage1, NewsImage2, NewsImage3];
+
+  const getImageForIndex = (index: number) => {
+    if (index % 2 === 0) {
+      return images[1];
+    }
+    if (index % 3 === 0) {
+      return images[2];
+    }
+    return images[0];
+  };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+
+    vertical: true,
+    verticalSwiping: true,
+
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 1500,
+    cssEase: "linear",
+    pauseOnHover: false,
+  };
+
+  return (
+    <ul className="slider-container !relative">
+      <Slider {...settings}>
+        {Array(8)
+          .fill(null)
+          .map((_, i) => (
+            <li
+              key={`card-${i}`}
+              className="!grid grid-cols-10 items-center gap-4 mt-4 bg-white/70 rounded-lg shadow-lg px-10 py-4">
+              <div className="col-span-1 flex flex-col items-start gap-3">
+                <p className="font-bold">27 Sep 2024</p>
+                <p className="font-medium">
+                  10:30 am <span className="block">-</span> 3:30 pm{" "}
+                </p>
+              </div>
+
+              <figure className="col-span-1">
+                <Image src={getImageForIndex(i)} alt="card-image" />
+              </figure>
+
+              <div className="col-span-7 flex flex-col items-stretch gap-1">
+                <h3 className="font-bold text-lg">Conference</h3>
+                <p className="text-lg w-5/6">
+                  Join industry leaders and innovators at our Tech Innovation
+                  Summit 2022 for insightful discussions, networking
+                  opportunities, and the latest technological trends and
+                  developments.
+                </p>
+              </div>
+
+              <Button className="col-span-1 bg-gradient-to-b from-custom-orange via-custom-orange to-custom-orange-dark">
+                Action
+              </Button>
+            </li>
+          ))}
+      </Slider>
+    </ul>
+  );
+}
+
+// Events component
 export default function Events(): React.JSX.Element {
   return (
-    <section>
-      <div>
-        <h2 className="text-2xl">
+    <section className="p-20 flex flex-col items-stretch gap-10 bg-light-custom-green/70">
+      <div className="flex flex-col items-center gap-6">
+        <h2 className="text-4xl text-center text-custom-green font-semibold flex flex-col items-center gap-3">
           <span>Events.</span>
-          <span className="w-5/6 h-1 bg-custom-orange" />
+          <span className="w-4/5 h-1 bg-custom-orange inline-block" />
         </h2>
-        <p>
+        <p className="w-2/3 text-center text-custom-green font-medium">
           Discover upcoming events, business conferences, trade shows, seminars,
           networking meets, and workshops. Additionally, a list of past events
           within the last 6 months.
         </p>
       </div>
+
+      <MultipleItems />
     </section>
   );
 }
