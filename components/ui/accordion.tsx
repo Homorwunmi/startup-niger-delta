cd607b6e00e08316bafaa913f8176b9c4fae2a9a
orisabiyi <orisabiyidavid@gmail.com>
2025-04-13 08:29:18 +0100

    update

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -36,9 +36,10 @@ function AccordionTrigger({
         data-slot="accordion-trigger"
         className={cn(
           "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
-          className
+          className,
         )}
-        {...props}>
+        {...props}
+      >
         {children}
         <div className="rounded-full bg-custom-green/60 flex items-center justify-center">
           <GoPlusCircle className="text-custom-green pointer-events-none rotate-90 transition-transform duration-200 w-6 h-6" />
@@ -57,7 +58,8 @@ function AccordionContent({
     <AccordionPrimitive.Content
       data-slot="accordion-content"
       className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
-      {...props}>
+      {...props}
+    >
       <div className={cn("pt-0 pb-4", className)}>{children}</div>
     </AccordionPrimitive.Content>
   );
