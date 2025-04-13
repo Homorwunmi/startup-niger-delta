3a3623209f3b07a90b599f6d22963341b3c3fee5
orisabiyi <orisabiyidavid@gmail.com>
2025-04-13 08:28:37 +0100

    feat: include the faqs section question and refactor reusable component code for improved structure

--- components/home/faq.tsx
+++ components/home/faq.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import React from "react";
 import QuestionMark from "@/public/images/question-mark.svg";
+import FaqMain from "@/public/images/faq-main.svg";
 import Image from "next/image";
+import { faqs } from "@/lib/home";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
 
 export default function FrequentlyAsked(): React.JSX.Element {
   return (
@@ -10,10 +20,9 @@ export default function FrequentlyAsked(): React.JSX.Element {
         style={{
           backgroundImage: `url("/home/ecosystem-bg.svg")`,
           backgroundBlendMode: "soft-light",
-        }}
-      >
-        <h2 className="text-4xl decoration-light-custom-green underline-offset-[15px] underline mb-4 flex flex-col items-start">
-          <span>FAQ.</span>
+        }}>
+        <h2 className="text-4xl decoration-light-custom-green underline-offset-[20px] underline mb-4 flex flex-col items-start">
+          FAQ.
         </h2>
         <p className="capitalize text-xl">Do you have questions?</p>
         <p className="text-3xl text-custom-green-4 font-semibold">
@@ -28,7 +37,27 @@ export default function FrequentlyAsked(): React.JSX.Element {
           />
         </figure>
       </div>
-      <div className="p-20 bg-white">Hello</div>
+
+      <div className="p-20 bg-white flex items-stretch">
+        <figure className="w-1/2">
+          <Image src={FaqMain} alt="faq-main" className="w-full h-auto" />
+        </figure>
+
+        <div className="flex-1">
+          {faqs.map((faq, i) => (
+            <Accordion type="single" collapsible key={`faqs-${i}`}>
+              <AccordionItem
+                value="item-1"
+                className="bg-white text-black rounded-lg shadow-md mb-6 px-5 !py-[.5px]">
+                <AccordionTrigger className="hover:no-underline">
+                  {faq.ques}
+                </AccordionTrigger>
+                <AccordionContent>{faq.ans}</AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
--- lib/home.ts
+++ lib/home.ts
@@ -80,3 +80,38 @@ export const resourcesList = [
     desc: "Connect, collaborate, innovate",
   },
 ];
+
+export const faqs = [
+  {
+    ques: "Q: What types of training programs are available on your platform?",
+    ans: "A: We offer a wide range of training programs tailored to enhance digital skills, including technical training, leadership development, and industry-specific workshops.",
+  },
+  {
+    ques: "Q: What opportunities are there for innovation and idea development on your platform?",
+    ans: "A: Our platform serves as an innovation incubator, providing a nurturing environment and resources to cultivate and grow tech innovations from concept to reality.",
+  },
+  {
+    ques: "Q: What role does your platform play in fostering the next generation of tech leaders?",
+    ans: "A: We provide programs and initiatives aimed at mentoring and guiding emerging tech talent, contributing to the development of future leaders in the digital technology space.",
+  },
+  {
+    ques: "Q: Are your training programs led by industry experts and professionals?",
+    ans: "A: Absolutely. Our training programs are curated and delivered by industry experts and professionals with extensive experience in the digital technology sector.",
+  },
+  {
+    ques: "Q: How does your platform support startups in scaling up their ventures?",
+    ans: "A: We provide targeted support and strategic guidance to help startups navigate the complexities of scaling up, offering resources, mentorship, & connections to accelerate growth.",
+  },
+  {
+    ques: "Q: How does your platform empower entrepreneurs in their journey?",
+    ans: "A: We offer mentorship opportunities, funding connections, and essential resources to empower entrepreneurs to succeed in the fast-paced tech industry.",
+  },
+  {
+    ques: "Q: Can individuals at varying stages of their tech careers benefit from your platform?",
+    ans: "A: Yes, our platform caters to individuals at all career stages, from aspiring professionals looking to enhance their skills to seasoned entrepreneurs seeking to scale their ventures.",
+  },
+  {
+    ques: "Q: How can I get involved with your platform and access these resources and opportunities?",
+    ans: "A: Joining our platform is simple. Sign up for an account to unlock access to our training programs, mentorship opportunities, funding connections, and a vibrant community of tech enthusiasts and innovators.",
+  },
+];
