02473633413075fe2ba9d9f1ac6fe9678edaad5c
orisabiyi <orisabiyidavid@gmail.com>
2025-04-13 22:20:01 +0100

    Add alert logo SVG, update TypeScript configuration, and declare Splide module types
    - Added a new SVG file for the alert logo in the public/images directory.
    - Updated tsconfig.json to skip default library checks for improved compatibility.
    - Created a new TypeScript declaration file for the '@splidejs/react-splide' module to define Splide and SplideSlide components.

--- components/home/alert.tsx
+++ components/home/alert.tsx
@@ -1,6 +1,52 @@
 'use client';
 
 import React from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+
+function SplideCenterMode() {
+  const splideOptions = {
+    type: 'loop',
+    focus: 'center',
+    perPage: 3,
+    gap: '1rem',
+    padding: '1rem',
+    arrows: true,
+    pagination: true,
+    autoplay: true,
+    interval: 3000,
+    pauseOnHover: false,
+    breakpoints: {
+      768: {
+        perPage: 1,
+        padding: '2rem',
+      },
+    },
+  };
+
+  return (
+    <Splide options={splideOptions}>
+      <SplideSlide>
+        <div className="bg-gray-500">Slide 1</div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="bg-gray-500">Slide 2</div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="bg-gray-500">Slide 3</div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="bg-gray-500">Slide 4</div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="bg-gray-500">Slide 5</div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className="bg-gray-500">Slide 6</div>
+      </SplideSlide>
+    </Splide>
+  );
+}
 
 export default function Alert(): React.JSX.Element {
   return (
@@ -15,6 +61,8 @@ export default function Alert(): React.JSX.Element {
         <h3 className="uppercase underline underline-offset-4">Category</h3>
         <h3 className="uppercase underline underline-offset-4">Year</h3>
       </div>
+
+      <SplideCenterMode />
     </section>
   );
 }
