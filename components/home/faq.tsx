eaa1d1ae4886255a7f5ad5141e5ab40c3e5ce080
orisabiyi <orisabiyidavid@gmail.com>
2025-04-13 08:54:56 +0100

    feat: add contact options to Frequently Asked Questions component with new mail icon

--- components/home/faq.tsx
+++ components/home/faq.tsx
@@ -3,8 +3,12 @@
 import React from "react";
 import QuestionMark from "@/public/images/question-mark.svg";
 import FaqMain from "@/public/images/faq-main.svg";
+import Mail from "@/public/images/mail.svg";
+
 import Image from "next/image";
 import { faqs } from "@/lib/home";
+import { FaPhoneAlt } from "react-icons/fa";
+import { RiAtLine } from "react-icons/ri";
 import {
   Accordion,
   AccordionContent,
@@ -14,20 +18,19 @@ import {
 
 export default function FrequentlyAsked(): React.JSX.Element {
   return (
-    <section className="text-white">
+    <section className="text-white relative">
       <div
         className="bg-custom-green p-20 h-96 font-poppins flex flex-col gap-2 relative"
         style={{
           backgroundImage: `url("/home/ecosystem-bg.svg")`,
           backgroundBlendMode: "soft-light",
-        }}
-      >
+        }}>
         <h2 className="text-4xl decoration-light-custom-green underline-offset-[20px] underline mb-4 flex flex-col items-start">
           FAQ.
         </h2>
         <p className="capitalize text-xl">Do you have questions?</p>
-        <p className="text-3xl text-custom-green-4 font-semibold">
-          Get in Touch
+        <p className="text-4xl text-custom-green-4 font-semibold">
+          Get in Touch.
         </p>
 
         <figure className="absolute bottom-1 left-0 w-full">
@@ -39,6 +42,23 @@ export default function FrequentlyAsked(): React.JSX.Element {
         </figure>
       </div>
 
+      <ul className="flex items-center gap-10 w-fit text-custom-green absolute top-1/5 left-20">
+        <li className="w-48 h-48 bg-gray-100 rounded-md shadow-lg pt-10 flex flex-col items-center justify-center gap-7 transition-all duration-300 hover:bg-light-custom-green">
+          <FaPhoneAlt size={45} />
+          <p className="text-lg font-medium">Call</p>
+        </li>
+        <li className="w-48 h-48 bg-gray-100 rounded-md shadow-lg pt-10 flex flex-col items-center justify-center gap-7 transition-all duration-300 hover:bg-light-custom-green">
+          <figure>
+            <Image src={Mail} alt="mail-icon" />
+          </figure>
+          <p className="text-lg font-medium">Email</p>
+        </li>
+        <li className="w-48 h-48 bg-gray-100 rounded-md shadow-lg pt-10 flex flex-col items-center justify-center gap-7 transition-all duration-300 hover:bg-light-custom-green">
+          <RiAtLine size={45} />
+          <p className="text-lg font-medium">DM</p>
+        </li>
+      </ul>
+
       <div className="p-20 bg-white flex items-stretch">
         <figure className="w-1/2">
           <Image src={FaqMain} alt="faq-main" className="w-full h-auto" />
@@ -49,8 +69,7 @@ export default function FrequentlyAsked(): React.JSX.Element {
             <Accordion type="single" collapsible key={`faqs-${i}`}>
               <AccordionItem
                 value="item-1"
-                className="bg-white text-black rounded-lg shadow-md mb-6 px-5 !py-[.5px]"
-              >
+                className="bg-white text-black rounded-lg shadow-md mb-6 px-5 !py-[.5px]">
                 <AccordionTrigger className="hover:no-underline">
                   {faq.ques}
                 </AccordionTrigger>
