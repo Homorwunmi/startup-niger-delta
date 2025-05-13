664e36ac142d833b760266cbdfc72ff90d9948de
Thoobi <sydneydaniel76@gmail.com>
2025-05-13 14:50:03 +0100

    chores: completes the document page

--- app/dashboard/documents/page.tsx
+++ app/dashboard/documents/page.tsx
@@ -1,6 +1,6 @@
 import { RiSearchLine } from 'react-icons/ri';
-import { LuThumbsUp } from 'react-icons/lu';
-import { FiFilter } from 'react-icons/fi';
+import { FiDownload } from 'react-icons/fi';
+import { DocumentTable } from '../../../components/dashboard/DocumentTable';
 
 export default function page() {
   return (
@@ -18,16 +18,18 @@ export default function page() {
             type="button"
             className="border-[#E7E6E6] bg-white border rounded-md px-3 py-2 flex items-center gap-2"
           >
-            <LuThumbsUp className="text-[#184341]" />
+            <FiDownload className="text-[#184341]" />
           </button>
           <button
             type="button"
-            className="border-[#E7E6E6] bg-white border rounded-md px-3 py-1 flex items-center gap-2"
+            className="bg-[#184341] border rounded-sm px-5 py-1 border-[#E7E6E6]"
           >
-            <FiFilter className="text-[#184341] " />
-            <h3 className="text-[#184341] text-base">Filter</h3>
+            <h3 className="text-white text-base">Upload file</h3>
           </button>
         </div>
+      </div>
+      <div className="px-11 py-3 w-full flex-1">
+        <DocumentTable />
       </div>
     </section>
   );
