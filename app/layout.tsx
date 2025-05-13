97ad0676bb5b1f6c109e238c0ad59605c36ace1f
orisabiyi <orisabiyidavid@gmail.com>
2025-05-13 17:09:22 +0100

    refactor: update layout to conditionally render Navbar and Footer based on pathname

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import React from 'react';
 import type { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/shared/navbar';
 import Footer from '@/components/shared/footer';
+import { usePathname } from 'next/navigation';
 import { OnboardingProvider } from './contexts/OnboardingContext';
 
 const inter = Inter({
@@ -21,15 +24,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
         <OnboardingProvider>
-          <>
-            <Navbar />
-            {children}
-            <Footer />
-          </>
+          {pathname.includes('dashboard') ? (
+            <>
+              <Navbar />
+              {children}
+              <Footer />
+            </>
+          ) : (
+            children
+          )}
         </OnboardingProvider>
       </body>
     </html>
