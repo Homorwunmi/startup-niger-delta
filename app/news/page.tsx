f43ae0797f21ab0b6a668e4096e476cd09948599
orisabiyi <orisabiyidavid@gmail.com>
2025-05-13 12:32:04 +0100

    feat: add recent news section with dynamic data rendering and styling

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,3 +1,74 @@
+import { Clock9 } from 'lucide-react';
+import Image from 'next/image';
+
+const data = [
+  {
+    id: 1,
+    title: 'How collaboration makes us better business person',
+    description:
+      'Lorem ipsum dolor sit amet consectetur.  Ont Condimentum adipiscing at iaculis m wqwa adiscing convallis ut feugiat morbi. Indo...',
+    author: 'By Admin, NDS',
+    date: new Date().toLocaleDateString('en-US', {
+      month: 'short',
+      year: 'numeric',
+      day: 'numeric',
+    }),
+    imageUrl: '/images/recent-1.svg',
+  },
+  {
+    id: 2,
+    title: 'How collaboration makes us better business person',
+    description:
+      'Lorem ipsum dolor sit amet consectetur.  Ont Condimentum adipiscing at iaculis m wqwa adiscing convallis ut feugiat morbi. Indo...',
+    author: 'By Admin, NDS',
+    date: new Date().toLocaleDateString('en-US', {
+      month: 'short',
+      year: 'numeric',
+      day: 'numeric',
+    }),
+    imageUrl: '/images/recent-2.svg',
+  },
+  {
+    id: 3,
+    title: 'How collaboration makes us better business person',
+    description:
+      'Lorem ipsum dolor sit amet consectetur.  Ont Condimentum adipiscing at iaculis m wqwa adiscing convallis ut feugiat morbi. Indo...',
+    author: 'By Admin, NDS',
+    date: new Date().toLocaleDateString('en-US', {
+      month: 'short',
+      year: 'numeric',
+      day: 'numeric',
+    }),
+    imageUrl: '/images/recent-3.svg',
+  },
+  {
+    id: 4,
+    title: 'How collaboration makes us better business person',
+    description:
+      'Lorem ipsum dolor sit amet consectetur.  Ont Condimentum adipiscing at iaculis m wqwa adiscing convallis ut feugiat morbi. Indo...',
+    author: 'By Admin, NDS',
+    date: new Date().toLocaleDateString('en-US', {
+      month: 'short',
+      year: 'numeric',
+      day: 'numeric',
+    }),
+    imageUrl: '/images/recent-4.svg',
+  },
+  {
+    id: 5,
+    title: 'How collaboration makes us better business person',
+    description:
+      'Lorem ipsum dolor sit amet consectetur.  Ont Condimentum adipiscing at iaculis m wqwa adiscing convallis ut feugiat morbi. Indo...',
+    author: 'By Admin, NDS',
+    date: new Date().toLocaleDateString('en-US', {
+      month: 'short',
+      year: 'numeric',
+      day: 'numeric',
+    }),
+    imageUrl: '/images/recent-5.svg',
+  },
+];
+
 export default function Page() {
   return (
     <main>
@@ -10,6 +81,66 @@ export default function Page() {
           Keep up to date with the latest news and updates.
         </p>
       </header>
+
+      <section className="flex items-stretch gap-4 px-20 py-4">
+        <div className="w-3/4">
+          <div>
+            <div>
+              <h2>Recent News</h2>
+            </div>
+
+            <ul className="grid grid-cols-6 gap-4">
+              {data.map((dta, i) =>
+                i === 0 || i === 1 ? (
+                  <li key={dta.id} className="col-span-3">
+                    <h3>{dta.title}</h3>
+                    <figure>
+                      <Image
+                        src={dta.imageUrl}
+                        alt="news"
+                        width={500}
+                        height={300}
+                      />
+                    </figure>
+                    <div className="flex items-center justify-between">
+                      <p>{dta.author}</p>
+                      <p className="flex items-center gap-2">
+                        <Clock9 />
+                        <span>{dta.date}</span>
+                        <span>- 2 min read</span>
+                      </p>
+                    </div>
+                    <p>{dta.description}</p>
+                  </li>
+                ) : (
+                  <li key={dta.id} className="col-span-2">
+                    <h3>{dta.title}</h3>
+                    <figure>
+                      <Image
+                        src={dta.imageUrl}
+                        alt="news"
+                        width={500}
+                        height={300}
+                      />
+                    </figure>
+                    <div className="flex items-center justify-between">
+                      <p>{dta.author}</p>
+                      <p className="flex items-center gap-2">
+                        <Clock9 />
+                        <span>{dta.date}</span>
+                        <span>- 2 min read</span>
+                      </p>
+                    </div>
+                    <p>{dta.description}</p>
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
+        </div>
+
+        <div>Hello</div>
+      </section>
     </main>
   );
 }
