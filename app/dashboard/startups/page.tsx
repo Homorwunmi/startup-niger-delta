c9b87cdd223183b427ea789cd7e6accf3cc3171a
orisabiyi <orisabiyidavid@gmail.com>
2025-06-18 08:29:48 +0100

    Revert "Remove dashboard layout and related components, including notifications, programs, scaleup, startups, and news pages. Delete sidebar and document table components, along with investor and startup card components to streamline the dashboard structure."
    This reverts commit 8c083bc50c9097d792bb3c07bde3b37f65e6ca87.

---
+++ app/dashboard/startups/page.tsx
@@ -0,0 +1,38 @@
+import { RiSearchLine } from 'react-icons/ri';
+import { LuThumbsUp } from 'react-icons/lu';
+import { FiFilter } from 'react-icons/fi';
+import { StartupCard } from '@/components/dashboard/StartupCard';
+
+export default function Startup() {
+  return (
+    <section className="flex-1 bg-[#F7FBFC] w-full h-full">
+      <div className="flex items-center justify-between px-11 py-3 w-full flex-1">
+        <h1 className="text-2xl font-semibold text-[#184341]">STARTUPS</h1>
+        <div className="flex items-center gap-4">
+          <button
+            type="button"
+            className="border-[#E7E6E6] bg-white border-[1.1px] rounded-md px-3 py-2 flex items-center gap-2"
+          >
+            <RiSearchLine className="text-[#184341]" />
+          </button>
+          <button
+            type="button"
+            className="border-[#E7E6E6] bg-white border rounded-md px-3 py-2 flex items-center gap-2"
+          >
+            <LuThumbsUp className="text-[#184341]" />
+          </button>
+          <button
+            type="button"
+            className="border-[#E7E6E6] bg-white border rounded-md px-3 py-1 flex items-center gap-2"
+          >
+            <FiFilter className="text-[#184341] " />
+            <h3 className="text-[#184341] text-base">Filter</h3>
+          </button>
+        </div>
+      </div>
+      <div className="px-11 py-3 w-full">
+        <StartupCard />
+      </div>
+    </section>
+  );
+}
