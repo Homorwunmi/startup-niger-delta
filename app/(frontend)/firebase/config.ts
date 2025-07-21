fa7ede83337b4eaadc9e3a17527c2118895135b8
orisabiyi <orisabiyidavid@gmail.com>
2025-07-21 14:28:59 +0100

    chore: fix api key

--- app/(frontend)/firebase/config.ts
+++ app/(frontend)/firebase/config.ts
@@ -1,10 +1,11 @@
+import { API_KEY } from 'constant';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 export const app = initializeApp({
-  apiKey: process.env.API_KEY,
+  apiKey: API_KEY,
   authDomain: 'nigerdeltastartup.firebaseapp.com',
   databaseURL: 'https://nigerdeltastartup-default-rtdb.firebaseio.com',
   projectId: 'nigerdeltastartup',
