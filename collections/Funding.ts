5d790a73ed5b645139ae64329263ac46e6a7cbfe
orisabiyi <orisabiyidavid@gmail.com>
2025-07-22 14:04:20 +0100

    feat: add Funding collection with type and category fields

---
+++ collections/Funding.ts
@@ -0,0 +1,41 @@
+import { CollectionConfig } from "payload";
+
+export const funding: CollectionConfig = {
+  slug: 'funding',
+  labels: {
+    singular: 'Funding',
+    plural: 'Funding',
+  },
+  fields: [
+    {
+      name: 'type',
+      type: 'select',
+      label: 'Funding Type',
+      options: [
+        { label: 'Angel Investors', value: 'angel-investors' },
+        { label: 'Venture Capitalists', value: 'venture-capitalists' },
+        { label: 'Corporate Venture Capital', value: 'corporate-venture-capital' },
+        { label: 'Seed Investors', value: 'seed-investors' },
+        { label: 'Accelerators', value: 'accelerators' },
+        { label: 'Incubators', value: 'incubators' },
+      ]
+    },
+    {
+      name: 'category',
+      type: 'select',
+      label: 'Category Type',
+      options: [
+        { label: 'Fintech', value: 'fintech' },
+        { label: 'E-commerce', value: 'e-commerce' },
+        { label: 'biotech', value: 'biotech' },
+        { label: 'AgriTech', value: 'agritech' },
+        { label: 'Health Tech', value: 'health-tech' },
+        { label: 'Edtech', value: 'edtech' },
+        { label: 'Artificial Intelligence', value: 'artificial-intelligence' },
+        { label: 'Gaming', value: 'gaming' },
+        { label: 'Robotics', value: 'robotics' },
+        { label: 'Others', value: 'others' },
+      ]
+    }
+  ]
+}
